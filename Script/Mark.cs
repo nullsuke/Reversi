05cf6a2a3de0b3c721e668cfb276fe4b60295a3f
Nullsuke <nullsuke@gmail.com>
2020-11-29 14:58:19 +0900

    合法手のマーク

---
+++ Script/Mark.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Mark : MonoBehaviour
+{
+    [SerializeField] Sprite[] colorSprites = default;
+
+    public enum ColorType { Black = 0, White = 1 }
+    protected RectTransform rectTransform;
+    private SpriteRenderer spriteRenderer;
+    private ColorType color;
+
+    public ColorType Color
+    {
+        get => color;
+        set
+        {
+            color = value;
+            spriteRenderer.sprite = colorSprites[(int)color];
+        }
+    }
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
