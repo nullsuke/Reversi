023166c96ab6fb5a4a8bb7a02e1acfd0ae682aaf
Nullsuke <nullsuke@gmail.com>
2020-11-29 14:53:41 +0900

    パスのメッセージを表示

---
+++ Script/PassPanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PassPanel : MonoBehaviour
+{
+    [SerializeField] Sprite[] passSprites = default;
+    private Image image;
+
+    public void Show(Stone.ColorType color)
+    {
+        image.sprite = passSprites[(int)color];
+    }
+
+    public void SetActive(bool value)
+    {
+        gameObject.SetActive(value);
+    }
+
+    public void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+}
