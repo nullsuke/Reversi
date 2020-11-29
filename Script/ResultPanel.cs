b9fef38f2b554fb01a0fd6d797a50b389fe1e5ad
Nullsuke <nullsuke@gmail.com>
2020-11-29 14:55:22 +0900

    勝敗のメッセージを表示

---
+++ Script/ResultPanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultPanel : MonoBehaviour
+{
+    [SerializeField] Image image = default;
+    [SerializeField] Sprite[] resultSprites = default;
+
+    public void Show(int n)
+    {
+        image.sprite = resultSprites[n];
+    }
+
+    public void SetActive(bool value)
+    {
+        gameObject.SetActive(value);
+    }
+}
